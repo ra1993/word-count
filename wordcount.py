623787be50031c2f1a5050895fe829bfd7cf3c87
Richard Amin <richardamin93@gmail.com>
2020-01-15 14:36:50 -0500

    update to code

--- wordcount.py
+++ wordcount.py
@@ -1,22 +1,41 @@
+import string
+
+def remove_punctuation(value):
+    result = ""
+    for p in value:
+        p = p.strip('“,”')
+        if p not in string.punctuation:
+            result += p
+    return result   
+
 
 def wordFreq(n):
-    punctuations = "!,(),-,[],"",;,:,',<,>,.,/,?,@,#,$,%,^,&,*,_,~"
 
     opened_file = open(n, 'r')
 
     wordcount = {}
+    top10 = []
 
     for line in opened_file:
+        line = line.lower()
+
+        line = remove_punctuation(line) #removes punctuations.....
         line = line.split(" ")
         
-
+    
         for i in line:
             if i in wordcount:
                  wordcount[i] += 1
             else:
                  wordcount[i] = 1
-            print(i, wordcount[i])
+
+    counter = 0
+    for k, v in sorted(wordcount.items(), reverse=False, key = lambda x: x[1]):
     
+        if counter <  10:
+            counter += 1
+        print(k, v)   
+
 
     opened_file.close() 
 
